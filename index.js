11cc062782669204526abc1bf71b79539e22d201
NguyenNTT-VN <153304794+NguyenNTT-VN@users.noreply.github.com>
2023-12-08 22:56:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,110 +1,90 @@
 
 // const { clientId, guildId, token, publicKey } = require('./config.json');
 require('dotenv').config()
-const APPLICATION_ID = process.env.APPLICATION_ID 
-const TOKEN = process.env.TOKEN 
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
 
 
 const axios = require('axios')
 const express = require('express');
-const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
 
 
 const app = express();
 // app.use(bodyParser.json());
 
-const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
-  timeout: 3000,
-  headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${TOKEN}`
-  }
-});
 
 
 
 
-app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
-  const interaction = req.body;
 
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(interaction.data.name)
-    if(interaction.data.name == 'yo'){
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `Yo ${interaction.member.user.username}!`,
-        },
-      });
-    }
+const OpenAI = require("openai") ;
+const prompt = require('prompt-sync')();
+const openai = new OpenAI({
+    apiKey: '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',
+  });
 
-    if(interaction.data.name == 'dm'){
-      // https://discord.com/developers/docs/resources/user#create-dm
-      let c = (await discord_api.post(`/users/@me/channels`,{
-        recipient_id: interaction.member.user.id
-      })).data
-      try{
-        // https://discord.com/developers/docs/resources/channel#create-message
-        let res = await discord_api.post(`/channels/${c.id}/messages`,{
-          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
-        })
-        console.log(res.data)
-      }catch(e){
-        console.log(e)
-      }
-
-      return res.send({
-        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data:{
-          content:'👍'
-        }
-      });
-    }
-  }
-
-});
-
-
-
-app.get('/register_commands', async (req,res) =>{
-  let slash_commands = [
-    {
-      "name": "yo",
-      "description": "replies with Yo!",
-      "options": []
-    },
-    {
-      "name": "dm",
-      "description": "sends user a DM",
-      "options": []
-    }
-  ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
-  }catch(e){
-    console.error(e.code)
-    console.error(e.response?.data)
-    return res.send(`${e.code} error from discord`)
-  }
+ 
+  const { Client, GatewayIntentBits } = require('discord.js')
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+   
+    ]
 })
 
+  client.once('ready', () => {
+      console.log(`Logged in as ${client.user.username}`);
+  });
+  let user = "NFI media"
+  let promptdata = `System: You are a good girl with name: Oatami, remember don't tell "${user}" you are an A.I when chat with him and you are "${user}"'s girlfriends always talking with ${user} using vietnamese\n `;
+  client.on('messageCreate', async (message) => {
+    if (message.author == client.user) return;
+    let name = message.content;
+     
+       console.log(message.content);
+       let prompt1 = promptdata + `"${user}": ${name} \n Oatami: `;
+    
+    
+       const stream = await openai.completions.create({
+           model: "gpt-3.5-turbo-instruct-0914",
+           max_tokens: 2000,
+           prompt: prompt1,
+       });
+       promptdata = promptdata + `"${user}": ${name} \n Oatami: ${stream.choices[0].text}\n`
+       console.log(stream.choices[0].text)
+       
+       if (name == "exit"){
+           process.exit()
+       }
 
-app.get('/', async (req,res) =>{
-  return res.send('Follow documentation ')
-})
+    await message.reply(stream.choices[0].text);
+      console.log()
+  });
+  
+  const TOKEN = 'MTE4MjY5NTgwNjA0NjgzODgyNA.GuEFg2.Lyy7KLhGdiM2Oj6cI0MRKLB1AOhd5OKkTRIQ4Y';
+  client.login(TOKEN);
+  
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+async function main(user, name) {
+   
+    
+    return stream.choices[0].text;
+}
+
+//main(user, promptdata);
 
 
 app.listen(8999, () => {
